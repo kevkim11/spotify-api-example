bccd55085d49772af5d2ae25086d84158cf40e0b
kevin_kim <kevkim1120@gmail.com>
2017-10-03 22:12:37 -0700

    can properly pass data as props from parent to child component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function Square(props) {
   return (
     <div className="flex-square">
-      <img src={props.imageUrl} alt=""></img>
+      <img src={props.item.album.images[1].url} alt=""/>
       <div className="overlay">
-        <p className="text">Song Name</p>
+        <div className="text">hello love</div>
       </div>
       {/*<img src={'https://i.scdn.co/image/9954c96d21486da541793eca1d7a338b45509f8c'} alt="boohoo"></img>*/}
     </div>
@@ -18,7 +18,7 @@ class Wall extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      songList: []  // my response.
+      currentItemList: [],
     };
     this.numOfSquare = 30; //numOfAlbums to retrieve. Max=50
   }
@@ -26,7 +26,7 @@ class Wall extends Component {
   componentDidMount(props) {
     // Why fetch in componentDidMount --> https://daveceddia.com/where-fetch-data-componentwillmount-vs-componentdidmount/
     console.log('this.state', this.state);
-    var accessToken = 'BQCW2JgibShiHG-Ipe1qyApvRxd0MFIfVPFN62HuQsJTvi1q2dg5zGtPoFC9jLJgbejGCDvC3Vj9oMwDWXgeVArdxpzVxOLqwyMUrOpZ-lqY2HXKsczli5XO8Sof4uUhxVJvGGi9TAGu4xiTH8AVzXNqvjncWDr4DUa2ySdY7DqILegfMH4';
+    var accessToken = 'BQCRpx80UDEkqTdql2ibRg_2CG8t9Rc_wB_AMs-LKEV3vicFNnxKwBSzl60tvxSI1jtyDVgBiKNfOMAc4whljTFVttlcg8FMkK8ORggEJLdLRFPLi7VZiLJJcwUdT6b5TsEpBrB9RuPeBfnFPLKnFlgaThQHrU6Zt-Mc_g3F_9VBv3mWpgQ';
     // this.getUserSongs(accessToken)
     // this.getUserAlbums(accessToken)
     this.getUserTopTracks(accessToken)
@@ -39,61 +39,61 @@ class Wall extends Component {
    * /v1/users/{user_id}/playlists/{playlist_id}/tracks
    * @param accessToken
    */
-  getUserSongs(accessToken) {
-    const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
-    const FETCH_URL = BASE_URL + 'tracks?limit=' + this.numOfSquare;
-    var myOptions = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      },
-      mode: 'cors',
-      cache: 'default'
-    };
+  // getUserSongs(accessToken) {
+  //   const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
+  //   const FETCH_URL = BASE_URL + 'tracks?limit=' + this.numOfSquare;
+  //   var myOptions = {
+  //     method: 'GET',
+  //     headers: {
+  //       'Authorization': 'Bearer ' + accessToken
+  //     },
+  //     mode: 'cors',
+  //     cache: 'default'
+  //   };
+  //
+  //   fetch(FETCH_URL, myOptions)
+  //     .then(response => response.json())
+  //     .then(json => {
+  //       console.log(json);
+  //       const songList = json.items;
+  //       console.log(songList[0]);
+  //       const temp_ar = [];
+  //       for (let i=0; i<songList.length; i+=1) {
+  //         temp_ar.push(songList[i].track.album.images[1].url)
+  //       }
+  //       this.setState({songList: temp_ar });
+  //       // const artist = json.artists.items[0];
+  //       // this.setState({ artist });
+  //     })
+  // }
 
-    fetch(FETCH_URL, myOptions)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        const songList = json.items;
-        console.log(songList[0]);
-        const temp_ar = [];
-        for (let i=0; i<songList.length; i+=1) {
-          temp_ar.push(songList[i].track.album.images[1].url)
-        }
-        this.setState({songList: temp_ar });
-        // const artist = json.artists.items[0];
-        // this.setState({ artist });
-      })
-  }
-
-  getUserAlbums(accessToken) {
-    const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
-    const FETCH_URL = BASE_URL + 'albums?limit=' + this.numOfSquare;
-    var myOptions = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      },
-      mode: 'cors',
-      cache: 'default'
-    };
-
-    fetch(FETCH_URL, myOptions)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        const albumList = json.items;
-        // console.log(songList[0]);
-        const temp_ar = [];
-        for (let i=0; i<albumList.length; i+=1) {
-          temp_ar.push(albumList[i].album.images[1].url)
-        }
-        this.setState({songList: temp_ar });
-        // const artist = json.artists.items[0];
-        // this.setState({ artist });
-      })
-  }
+  // getUserAlbums(accessToken) {
+  //   const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
+  //   const FETCH_URL = BASE_URL + 'albums?limit=' + this.numOfSquare;
+  //   var myOptions = {
+  //     method: 'GET',
+  //     headers: {
+  //       'Authorization': 'Bearer ' + accessToken
+  //     },
+  //     mode: 'cors',
+  //     cache: 'default'
+  //   };
+  //
+  //   fetch(FETCH_URL, myOptions)
+  //     .then(response => response.json())
+  //     .then(json => {
+  //       console.log(json);
+  //       const albumList = json.items;
+  //       // console.log(songList[0]);
+  //       const temp_ar = [];
+  //       for (let i=0; i<albumList.length; i+=1) {
+  //         temp_ar.push(albumList[i].album.images[1].url)
+  //       }
+  //       this.setState({songList: temp_ar });
+  //       // const artist = json.artists.items[0];
+  //       // this.setState({ artist });
+  //     })
+  // }
 
   getUserTopTracks(accessToken) {
     const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
@@ -113,60 +113,71 @@ class Wall extends Component {
         console.log(json);
         const songList = json.items;
         console.log(songList[0]);
-        const temp_ar = [];
-        for (let i=0; i<songList.length; i+=1) {
-          temp_ar.push(songList[i].album.images[1].url)
-        }
-        this.setState({songList: temp_ar });
+        this.setState({currentItemList: songList});
+        // this.setState({songList: temp_ar, artistList: temp_ar2 });
       })
   }
 
-  getUserRecentlyPlayedTracks(accessToken) {
-    const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
-    const FETCH_URL = BASE_URL + 'albums?limit=' + this.numOfSquare;
-    var myOptions = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      },
-      mode: 'cors',
-      cache: 'default'
-    };
-
-    fetch(FETCH_URL, myOptions)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        const albumList = json.items;
-        // console.log(songList[0]);
-        const temp_ar = [];
-        for (let i=0; i<albumList.length; i+=1) {
-          temp_ar.push(albumList[i].album.images[1].url)
-        }
-        this.setState({songList: temp_ar });
-        // const artist = json.artists.items[0];
-        // this.setState({ artist });
-      })
-  }
+  // getUserRecentlyPlayedTracks(accessToken) {
+  //   const BASE_URL = 'https://api.spotify.com/v1/me/'; //https://api.spotify.com/v1/albums/
+  //   const FETCH_URL = BASE_URL + 'albums?limit=' + this.numOfSquare;
+  //   var myOptions = {
+  //     method: 'GET',
+  //     headers: {
+  //       'Authorization': 'Bearer ' + accessToken
+  //     },
+  //     mode: 'cors',
+  //     cache: 'default'
+  //   };
+  //
+  //   fetch(FETCH_URL, myOptions)
+  //     .then(response => {
+  //       if(!response.ok) {
+  //         throw Error("Network Request Failed")
+  //       }
+  //       return response
+  //     })
+  //     .then(response => response.json())
+  //     .then(json => {
+  //       console.log(json);
+  //       const albumList = json.items;
+  //       // console.log(songList[0]);
+  //       const temp_ar = [];
+  //       for (let i=0; i<albumList.length; i+=1) {
+  //         temp_ar.push(albumList[i].album.images[1].url)
+  //       }
+  //       this.setState({songList: temp_ar });
+  //       // const artist = json.artists.items[0];
+  //       // this.setState({ artist });
+  //     })
+  // }
 
   render() {
+    var itemNodes = this.state.currentItemList.map((item, i) => {
+      return (
+        <Square item={item} key={i}/>
+      )
+    });
+
+    console.log("!!!!this.state.currentItemList: " );
+    console.log(this.state.currentItemList);
+    if(this.state.currentItemList.length === 0){return <p> {'loading...'} </p>}
     return (
       <div className="flex-container wrap">
-        {this.state.songList.map((url, i) => <Square imageUrl={url} key={i}/>)}
+      {itemNodes}
       </div>
     )
   }
-
 }
-
+{/*{this.state.currentItemList.map((currentItem, i) => <Square imageUrl={currentItem} key={i}/>)}*/}
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      'currentState': 'songs'
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     'currentState': 'songs'
+  //   }
+  // }
 
   render() {
 
